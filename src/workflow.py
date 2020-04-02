dd8eba958ad97492dbdfa946a53352d7bc193b69
Or Zilberman <zilbermanor@gmail.com>
2020-04-02 07:22:44 +0000

    -Updated to new mlrun project - Using MLRun Functions

--- src/workflow.py
+++ src/workflow.py
@@ -1,44 +1,27 @@
 from kfp import dsl
-import kfp
 from mlrun import mount_v3io
-import urllib
-import json
 import os
 
 funcs = {}
-lgbm_url = urllib.request.urlopen('https://raw.githubusercontent.com/yjb-ds/lightgbm-project/pre-project/lightgbm-conf.json')
-LGBM = json.load(lgbm_url)
-project_dir = '/User/demo-network-operations'
 
-def init_functions(functions: dict, params=None, secrets=None):
-    '''
-    This function will run before running the project.
-    It allows us to add our specific system configurations to the functions
-    like mounts or secrets if needed.
-
-    In this case we will add Iguazio's user mount to our functions using the
-    `mount_v3io()` function to automatically set the mount with the needed
-    variables taken from the environment. 
-    * mount_v3io can be replaced with mlrun.platforms.mount_pvc() for 
-    non-iguazio mount
-
-    @param functions: <function_name: function_yaml> dict of functions in the
-                        workflow
-    @param params: parameters for the function configurations
-    @param secrets: secrets required for the functions for s3 connections and
-                    such
-    '''
+def init_functions(functions: dict, project=None, secrets=None):
     for f in functions.values():
-        f.apply(mount_v3io())                  # On Iguazio (Auto-mount /User)
-        # f.apply(mlrun.platforms.mount_pvc()) # Non-Iguazio mount
-
-
-@kfp.dsl.pipeline(
+        # Add V3IO Mount
+        f.apply(mount_v3io())
+        
+        # Always pull images to keep updates
+        f.spec.image_pull_policy = 'Always'
+        
+    functions['aggregate'].spec.image = 'mlrun/ml-models:0.4.6'
+    for fn, fv in functions.items():
+        print(f'Function: {fn}')
+        print(fv.spec)
+@dsl.pipeline(
     name='Network Operations Demo',
     description='Train a Failure Prediction LGBM Model over sensor data'
 )
 def kfpipeline(
-        df_artifact = os.path.join(project_dir, 'data', 'metrics.pq'),
+        df_artifact = 'store://network-operations/metrics',
         metrics = ['cpu_utilization'],
         labels = ['is_error'],
         metric_aggs = ['mean', 'sum'],
@@ -55,9 +38,18 @@ def kfpipeline(
         TEST_SIZE        = 0.1,       # 10% set aside
         TRAIN_VAL_SPLIT  = 0.75,      # remainder split into train and val
         RNG              = 1,
-        class_params = LGBM['CLASS_PARAMS'],
-        fit_params = LGBM['FIT_PARAMS'],
+        config_filepath = 'store://network-operations/lgb_configs',
     ):
+    
+    describe = funcs['describe'].as_step(name='describe-raw-data',
+                                                handler="summarize",  
+                                                params={"key": "summary", 
+                                                        "label_column": "is_error", 
+                                                        'class_labels': ['0', '1'],
+                                                        'plot_hist': True,
+                                                        'plot_dest': 'plots'},
+                                                inputs={"table": df_artifact},
+                                                outputs=["summary", "scale_pos_weight"])
     
     # Run preprocessing on the data
     aggregate = funcs['aggregate'].as_step(name='aggregate',
@@ -73,30 +65,31 @@ def kfpipeline(
                                                           'save_to': save_to},
                                                   outputs=['aggregate'],
                                                   handler='aggregate',
-                                                  image='docker-registry.default-tenant.app.cnyidfihnjsz.iguazio-cd0.com:80/mlrun/func-default-aggregate-latest')
+                                                  image='mlrun/mlrun')
 
-    describe = funcs['describe'].as_step(name='describe',
-                                                handler="table_summary",  
-                                                params={"key": describe_table, 
-                                                        "label_column": label_column, 
-                                                        'class_labels': class_labels},
+    describe = funcs['describe'].as_step(name='describe-feature-vector',
+                                                handler="summarize",  
+                                                params={"key": "summary", 
+                                                        "label_column": "is_error", 
+                                                        'class_labels': ['0', '1'],
+                                                        'plot_hist': True,
+                                                        'plot_dest': 'plots'},
                                                 inputs={"table": aggregate.outputs['aggregate']},
-                                                outputs=["summary", "scale_pos_weight"]).after(aggregate)
+                                                outputs=["summary", "scale_pos_weight"])
     
-    train = funcs['train_lgbm'].as_step(name='train',
-                                               handler='train_model',
-                                               params={'sample'          : SAMPLE_SIZE,
-                                                       'label_column'    : label_column,
-                                                       'test_size'       : TEST_SIZE,
-                                                       'train_val_split' : TRAIN_VAL_SPLIT,
-                                                       'rng'             : RNG,
-                                                       'class_params'    : class_params,
-                                                       'fit_params'      : fit_params},
-                                               inputs={"data_key": aggregate.outputs['aggregate'],
-                                                        "scale_pos_weigth": describe.outputs["scale_pos_weight"]},
-                                               outputs=['model', 'test-set'])
+    train = funcs['train'].as_step(name='train', 
+                                          handler='train_model',
+                                          params={'model_pkg_class' : config_filepath,
+                                                  'sample'          : -1,
+                                                  'label_column'    : "is_error",
+                                                  'test_size'       : 0.10,
+                                                  'train_val_split' : 0.75,
+                                                  'rng'             : 1},
+                                          inputs={"data_key": aggregate.outputs['aggregate']},
+                                          outputs=['model', 'test-set'])
     
-
+#     test = funcs['test'].as_step()
+    
     # deploy the model using nuclio functions
     deploy = funcs['serving'].deploy_step(project='nuclio-serving',
                                                  models={'predictor': train.outputs['model']})
