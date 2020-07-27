085c2a57c3e0fbad1422898f025ad8e576d46261
Or Zilberman <Ziilbermanor@gmail.com>
2020-07-27 11:11:26 +0000

    Updated workflow to final version

--- src/workflow.py
+++ src/workflow.py
@@ -5,41 +5,22 @@ from nuclio.triggers import V3IOStreamTrigger
 
 funcs = {}
 projdir = os.getcwd()
-label_column = 'is_error'
-model_inference_stream = '/users/admin/demo-network-operations/streaming/inference_stream'
+# mlconf.hub_url = 'https://raw.githubusercontent.com/mlrun/functions/{tag}/{name}/function.yaml'
+mlconf.hub_url = '/User/functions/{name}/function.yaml'
+model_inference_stream = '/users/admin/demo-network-operations/streaming/predictions'
 model_inference_url = f'http://v3io-webapi:8081{model_inference_stream}'
-
+labeled_stream = 'http://v3io-webapi:8081/users/admin/demo-network-operations/streaming/labeled_stream'
 
 def init_functions(functions: dict, project=None, secrets=None):
-    tags = {'ml-models': 'unstable-py36',
-            'mlrun': 'unstable'}
     for f in functions.values():
         # Add V3IO Mount
         f.apply(mount_v3io())
-        if hasattr(f.spec, 'image'):
-            if not f.spec.image.endswith('unstable-py36') and (not f.spec.image == ''):
-                current_image = f.spec.image.split(':')[0]
-                fn_image = f"{current_image}:{tags[current_image.split('/')[1]]}"
-                print(fn_image)
-                f.spec.image = fn_image
         
         # Always pull images to keep updates
         f.spec.image_pull_policy = 'Always'
-        
-    functions['serving'].metadata.name = 'netops-server'
-    functions['serving'].spec.min_replicas = 1
-    functions['serving'].spec.build.baseImage = 'mlrun/mlrun:unstable'
     
     # Define inference-stream related triggers
-    functions['serving'].set_envs({'INFERENCE_STREAM': model_inference_stream})
-    functions['s2p'].add_trigger('labeled_stream', V3IOStreamTrigger(url=f'{model_inference_url}@s2p'))
-    functions['s2p'].set_envs({'window': 10,
-                               'features': 'cpu_utilization',
-                               'save_to': '/User/demo-network-operations/streaming/inference_pq/',
-                               'base_dataset': '/User/demo-network-operations/artifacts/test_set_preds.parquet',
-                               'hub_url': '/User/functions/{name}/function.yaml',
-                               'mount_path': '~/',
-                               'mount_remote': '/User'})
+    functions['s2p'].add_trigger('labeled_stream', V3IOStreamTrigger(url=f'{labeled_stream}@s2p'))
                 
         
 @dsl.pipeline(
@@ -47,27 +28,55 @@ def init_functions(functions: dict, project=None, secrets=None):
     description='Train a Failure Prediction LGBM Model over sensor data'
 )
 def kfpipeline(
+        # aggregate
         df_artifact = '/User/demo-network-operations/data/metrics.pq',
         metrics = ['cpu_utilization', 'throughput', 'packet_loss', 'latency'],
         metric_aggs = ['mean', 'sum', 'std', 'var', 'min', 'max', 'median'],
         suffix = 'daily',
-        window = 20,
+        window = 10,
+
+        # describe
         describe_table = 'netops',
         describe_sample = 0.3,
-        label_column = label_column,
+        label_column = 'is_error',
         class_labels = [1, 0],
-        SAMPLE_SIZE      = -1, # -n for random sample of n obs, -1 for entire dataset, +n for n consecutive rows
-        TEST_SIZE        = 0.1,       # 10% set aside
-        TRAIN_VAL_SPLIT  = 0.75,      # remainder split into train and val
-        aggregate_fn_url = '/User/functions/aggregate/function.yaml',
-        streaming_metrics_table = '/User/demo-network-operations/streaming/metrics',
+        plot_hist = True,
+    
+        # Feature selection
+        k = 5,
+        min_votes = 3,
+    
+        # Train
+        sample_size      = -1,        # -n for random sample of n obs, -1 for entire dataset, +n for n consecutive rows
+        test_size        = 0.1,       # 10% set aside
+        train_val_split  = 0.75,      # remainder split into train and val
+    
+        # Test
+        predictions_col = 'predictions',
+    
+        # Deploy
+        deploy_streaming = True,
+        aggregate_fn_url = 'hub://aggregate',
         streaming_features_table = '/User/demo-network-operations/streaming/features',
         streaming_predictions_table = '/User/demo-network-operations/streaming/predictions',
+    
+        # Streaming
+        streaming_metrics_table = '/User/demo-network-operations/streaming/metrics',
+    
+        # labeled stream creator
         streaming_labeled_table = '/users/admin/demo-network-operations/streaming/labeled_stream',
-        predictions_col = 'predictions',
+        
+        # Concept drift
+        deploy_concept_drift = True,
         secs_to_generate = 10,
-        deploy_streaming = True,
-        deploy_concept_drift = True
+        concept_drift_models = ['ddm', 'eddm', 'pagehinkley'],
+        output_tsdb = '/User/demo-network-operations/streaming/drift_tsdb',
+        input_stream = 'http://v3io-webapi:8081/users/admin/demo-network-operations/streaming/labeled_stream',
+        output_stream = '/User/demo-network-operations/streaming/drift_stream',
+        streaming_parquet_table =  '/User/demo-network-operations/streaming/inference_pq/',
+    
+        # Virtual drift
+        results_tsdb_table = 'admin/demo_network_operations/streaming/drift_magnitude'
     ):
     
     # Run preprocessing on the data
@@ -75,18 +84,18 @@ def kfpipeline(
                                                   params={'metrics': metrics,
                                                           'metric_aggs': metric_aggs,
                                                           'suffix': suffix,
-                                                          },
+                                                          'window': window},
                                                   inputs={'df_artifact': df_artifact},
                                                   outputs=['aggregate'],
                                                   handler='aggregate',
-                                                  image='mlrun/ml-models:unstable-py36')
+                                                  image='mlrun/ml-models')
 
     describe = funcs['describe'].as_step(name='describe-aggregation',
                                                 handler="summarize",  
                                                 params={"key": f"{describe_table}_aggregate", 
                                                         "label_column": label_column, 
                                                         'class_labels': class_labels,
-                                                        'plot_hist': True,
+                                                        'plot_hist': plot_hist,
                                                         'plot_dest': 'plots/aggregation',
                                                         'sample': describe_sample},
                                                 inputs={"table": aggregate.outputs['aggregate']},
@@ -94,37 +103,38 @@ def kfpipeline(
     
     feature_selection = funcs['feature_selection'].as_step(name='feature_selection',
                                                            handler='feature_selection',
-                                                           params={'k': 5,
-                                                                   'min_votes': 3,
+                                                           params={'k': k,
+                                                                   'min_votes': min_votes,
                                                                    'label_column': label_column},
                                                            inputs={'df_artifact': aggregate.outputs['aggregate']},
                                                            outputs=['feature_scores', 
                                                                     'max_scaled_scores_feature_scores'
                                                                     'selected_features_count', 
                                                                     'selected_features'],
-                                                           image='mlrun/ml-models:unstable-py36')
+                                                           image='mlrun/ml-models')
     
     describe = funcs['describe'].as_step(name='describe-feature-vector',
                                             handler="summarize",  
                                             params={"key": f'{describe_table}_features', 
                                                     "label_column": label_column, 
                                                     'class_labels': class_labels,
-                                                    'plot_hist': True,
+                                                    'plot_hist': plot_hist,
                                                     'plot_dest': 'plots/feature_vector'},
                                             inputs={"table": feature_selection.outputs['selected_features']},
                                             outputs=["summary", "scale_pos_weight"])
     
     train = funcs['train'].as_step(name='train',
-                                   params={"sample"          : SAMPLE_SIZE, 
+                                   params={"sample"          : sample_size, 
                                            "label_column"    : label_column,
-                                           "test_size"       : TEST_SIZE},
+                                           "test_size"       : test_size,
+                                           "train_val_split" : train_val_split},
                                    inputs={"dataset"         : feature_selection.outputs['selected_features']},
                                    hyperparams={'model_pkg_class': ["sklearn.ensemble.RandomForestClassifier", 
                                                                     "sklearn.linear_model.LogisticRegression",
                                                                     "sklearn.ensemble.AdaBoostClassifier"]},
                                    selector='max.accuracy',
                                    outputs=['model', 'test_set'],
-                                   image='mlrun/ml-models:unstable-py36')
+                                   image='mlrun/ml-models')
     
     test = funcs['test'].as_step(name='test',
                                  handler='test_classifier',
@@ -132,32 +142,35 @@ def kfpipeline(
                                          'predictions_column': predictions_col},
                                  inputs={'models_path': train.outputs['model'],
                                          'test_set': train.outputs['test_set']},
-                                 image='mlrun/ml-models:unstable-py36')
-    
-    # deploy the model using nuclio functions
-    deploy = funcs['serving'].deploy_step(env={'model_path': train.outputs['model'],
-                                               'FEATURES_TABLE': streaming_features_table,
-                                               'PREDICTIONS_TABLE': streaming_predictions_table,
-                                               'prediction_col': predictions_col}, 
-                                          tag='v1')
-    
-    # test out new model server (via REST API calls)
-    tester = funcs["model_server-tester"].as_step(name='model-tester',
-                                                  params={'addr': deploy.outputs['endpoint'], 
-                                                          'model': "predictor",
-                                                          'label_column': label_column},
-                                                  inputs={'table': train.outputs['test_set']})
+                                 outputs=['test_set_preds'],
+                                 image='mlrun/ml-models')
+
     
     with dsl.Condition(deploy_streaming == True):
+        
+        # deploy the model using nuclio functions
+        deploy = funcs['serving'].deploy_step(env={'model_path': train.outputs['model'],
+                                                   'FEATURES_TABLE': streaming_features_table,
+                                                   'PREDICTIONS_TABLE': streaming_predictions_table,
+                                                   'prediction_col': predictions_col}, 
+                                              tag='v1')
+
+        # test out new model server (via REST API calls)
+        tester = funcs["model_server-tester"].as_step(name='model-tester',
+                                                      params={'addr': deploy.outputs['endpoint'], 
+                                                              'model': "predictor",
+                                                              'label_column': label_column},
+                                                      inputs={'table': train.outputs['test_set']},
+                                                      outputs=['test_set_preds'])
     
         # Streaming demo functions
-        preprocessor = funcs['preprocessor'].deploy_step(env={ 'aggregate_fn_url': aggregate_fn_url,
+        preprocessor = funcs['create_feature_vector'].deploy_step(env={ 'aggregate_fn_url': aggregate_fn_url,
                                                                 'METRICS_TABLE': streaming_metrics_table,
                                                                 'FEATURES_TABLE': streaming_features_table,
                                                                 'metrics': metrics,
                                                                 'metric_aggs': metric_aggs,
                                                                 'suffix': suffix,
-                                                                'base_dataset': '/User/demo-network-operations/artifacts/selected_features.parquet',
+                                                                'base_dataset': train.outputs['test_set'],
                                                                 'label_col': label_column}).after(tester)
 
         labeled_stream_creator = funcs['labeled_stream'].deploy_step(env={'METRICS_TABLE': streaming_metrics_table,
@@ -174,22 +187,22 @@ def kfpipeline(
             concept_builder = funcs['concept_drift'].deploy_step(skip_deployed=True)
 
             concept_drift = funcs['concept_drift'].as_step(name='concept_drift_deployer',
-                                                           params={'models': ['ddm', 'eddm', 'pagehinkley'],
+                                                           params={'models': concept_drift_models,
                                                                    'label_col': label_column,
                                                                    'prediction_col': predictions_col,
-                                                                   'hub_url': '/User/functions/{name}/function.yaml',
-                                                                   'output_tsdb': '/User/network-operations/streaming/drift_tsdb',
-                                                                   'input_stream': 'http://v3io-webapi:8081/users/admin/network-operations/streaming/labeled_stream@cd2',
-                                                                   'output_stream': '/User/network-operations/streaming/drift_stream'},
-                                                           inputs={'base_dataset': '/User/demo-network-operations/artifacts/test_set_preds.parquet'},
+                                                                   'output_tsdb': output_tsdb,
+                                                                   'input_stream': f'{input_stream}@cds',
+                                                                   'output_stream': output_stream},
+                                                           inputs={'base_dataset': test.outputs['test_set_preds']},
                                                            artifact_path=mlconf.artifact_path,
                                                            image=concept_builder.outputs['image']).after(labeled_stream_creator)
 
             s2p = funcs['s2p'].deploy_step(env={'window': 10,
-                                                'features': 'cpu_utilization',
-                                                'save_to': '/User/demo-network-operations/streaming/inference_pq/',
-                                                'base_dataset': '/User/demo-network-operations/artifacts/test_set_preds.parquet',
-                                                'hub_url': '/User/functions/{name}/function.yaml',
-                                                'mount_path': '~/',
+                                                'features': metrics,
+                                                'save_to': streaming_parquet_table,
+                                                'base_dataset': test.outputs['test_set_preds'],
+                                                'results_tsdb_container': 'users',
+                                                'results_tsdb_table': results_tsdb_table,
+                                                'mount_path': '/users/admin',
                                                 'mount_remote': '/User'}).after(tester)
     
